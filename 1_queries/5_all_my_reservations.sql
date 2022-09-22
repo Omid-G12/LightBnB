e1a4d848b4408cf2306e408fb9ba786a6d4335b6
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-22 18:44:12 +0000

    more queries

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,8 @@
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
